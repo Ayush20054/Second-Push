214dec7ae71c58e96f692a891887fc1fade33a6d
Ayush Khandekar <ayushkhandekar33@gmail.com>
2026-01-22 21:30:35 +0530

    one loop is added

--- src/com/Ayush/Main.java
+++ src/com/Ayush/Main.java
@@ -3,6 +3,9 @@ package com.Ayush;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// creating one loop
+        for (int i=1;i<=10;i++){
+            System.out.println(i+" ");
+        }
     }
 }
